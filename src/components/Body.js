25608421c25c71cf6384b637d11e8b93931e3d93
monika131001 <monikasavhad78@gmail.com>
2026-02-24 22:42:29 +0530

    Separated the components into separate files

---
+++ src/components/Body.js
@@ -0,0 +1,36 @@
+import { useState } from "react";
+import RestaurantCard from "./RestaurantCard";
+import resList from "../utils/mockData";
+
+const Body = () => {
+  const [listOfRestaurants, setListOfRestaurants] = useState(resList);
+
+  return (
+    <div className="body">
+      <div className="filter">
+        <button className="filter_btn" onClick={() => {
+            const filteredList = listOfRestaurants.filter(
+              (res) => res.info.avgRating > 4,
+            );
+            setListOfRestaurants(filteredList);
+          }}
+        >
+          Top Rated Restaurants
+        </button>
+      </div>
+
+      {/* <div className="search">
+        <input className="search-input" placeholder="Search here..." />
+        <button>Search</button>
+      </div> */}
+
+      <div className="res-container">
+        {listOfRestaurants.map((restaurant) => (
+          <RestaurantCard key={restaurant.info.id} resData={restaurant} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Body;
